69df05fd81b32073600c50542eca90cf6423762f
Maxim Sokhatsky <maxim@synrc.com>
2013-01-16 03:55:51 +0200

    test data for Node, D, Go

---
+++ rate.R
@@ -0,0 +1,36 @@
+autos_data <- read.csv(file="webcontest.csv", header=TRUE, sep=",")
+max_y <- 30 # max(autos_data)
+
+plot_colors <- c("blue","red","forestgreen","black","orange","green","darkblue")
+
+png(filename="connections.png", height=600, width=600, bg="white")
+
+plot(autos_data$nginx,    type="o", col=plot_colors[1], ylim=c(0,30000), axes=F, ann=F)
+plot(autos_data$cowboy,   type="o", col=plot_colors[2], ylim=c(0,30000), axes=F, ann=F)
+plot(autos_data$warp,     type="o", col=plot_colors[3], ylim=c(0,30000), axes=F, ann=F)
+plot(autos_data$ocsigen,  type="o", col=plot_colors[4], ylim=c(0,30000), axes=F, ann=F)
+plot(autos_data$nodejs,   type="o", col=plot_colors[5], ylim=c(0,30000), axes=F, ann=F)
+plot(autos_data$vibe,     type="o", col=plot_colors[6], ylim=c(0,30000), axes=F, ann=F)
+plot(autos_data$gohttp,   type="o", col=plot_colors[7], ylim=c(0,30000), axes=F, ann=F)
+
+title(main="Cowboy/Warp/Ocsigen/NodeJS/Vibe/GoHttp", col.main="red", font.main=1)
+title(xlab= "Socket Connections", col.lab=rgb(0,0.5,0))
+title(ylab= "Requests/Sec", col.lab=rgb(0,0.5,0))
+
+axis(1, at=1:30, lab=c("1K",  "2K",  "3K",  "4K",  "5K",  "6K",  "7K",  "8K",  "9K",  "1K0",
+                       "11K", "12K", "13K", "14K", "15K", "16K", "17K", "18K", "19K", "2K0",
+                       "21K", "22K", "23K", "24K", "25K", "26K", "27K", "28K", "29K", "3K0"))
+
+axis(2, las=1, cex.axis=1000)
+
+lines(autos_data$nginx,    type="o", pch=21, lty=1, col=plot_colors[1])
+lines(autos_data$cowboy,   type="o", pch=25, lty=6, col=plot_colors[2])
+lines(autos_data$warp,     type="o", pch=22, lty=3, col=plot_colors[3])
+lines(autos_data$ocsigen,  type="o", pch=23, lty=4, col=plot_colors[4])
+lines(autos_data$nodejs,   type="o", pch=24, lty=5, col=plot_colors[5])
+lines(autos_data$vibe,     type="o", pch=21, lty=6, col=plot_colors[6])
+lines(autos_data$gohttp,   type="o", pch=21, lty=7, col=plot_colors[7])
+
+legend("topleft", c("ngnix","cowboy","warp","ocsigen","nodejs","vibe","gohttp"), cex=1, col=plot_colors, pch=20:25, lty=1:7);
+
+dev.off()
--- wrkwrap.pl
+++ wrkwrap.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 
 my $server = shift @ARGV || "localhost";
-my $port = shift @ARGV || "3000";
+my $port = shift @ARGV || "8888";
 my $path = shift @ARGV || "/";
 
 my $cmdline = "-t4 http://$server:$port/";
@@ -9,21 +9,20 @@ my @measurement = ();
 
 print "Measuring \"$cmdline\", please wait...\n";
 
-for (my $rate = 30000; $rate >= 1000; $rate -= 1000) {
+for (my $rate = 1000; $rate <= 30000; $rate += 1000) {
     my @attempts = map {
-	    my($rps, $err) = measure($_, $rate);
-	    { rps => $rps, err => $err }
-	} (1, 2);
+        my($rps, $err) = measure($_, $rate);
+        { rps => $rps, err => $err }
+    } (1,2);
     push(@measurement, {
-	rate => $rate,
-	attempts => \@attempts
+        rate => $rate,
+        attempts => \@attempts
     });
 }
 
 print "Rate,reached,delta,expected,ERate,A1,A2,A3,E1,E2,E3\n";
 
 foreach my $mnt (@measurement) {
-    print "$mnt->{rate}";
     my @attempts = @{$mnt->{attempts}};
     my $rpssum = 0;
     my $errsum = 0;
@@ -33,12 +32,10 @@ foreach my $mnt (@measurement) {
     if ( $mnt->{rate} > $rpssum/scalar(@attempts) )
                  { $delta = int($rpssum/scalar(@attempts)); } else { $delta = $mnt->{rate}; };
 
+    print "$mnt->{rate}";
+    print "," . int($rpssum/scalar(@attempts));
     print "," . $delta;
-    print "," . int($errsum*100/scalar(@attempts));
-    print "," . $mnt->{rate};
     print "," . int($errsum/scalar(@attempts));
-    print "," . $rpslist;
-    print "," . $errlist;
     print "\n";
 }
 
@@ -47,17 +44,17 @@ sub measure($) {
     my $rate = shift;
     my $conns = $rate;
     my $rps = 0;
-    my $err = 100;
-    my $cmd = "wrk -k -r 100k -c $conns $cmdline";
+    my $err = 0;
+    my $cmd = "wrk -k -r 30k -c $conns $cmdline";
     local $_;
     print "Executing $cmd\n";
-    open(I, "$cmd 2>/dev/null |") or die "Can't open wrk: $!";
+#    sleep(int($conns/1000));
+    open(I, "$cmd |") or die "Can't open wrk: $!";
     while(<I>) {
-	$rps = $1 if /^Requests\/sec:  (\d+)/;
-	$err = $1 if /^timeout (\d+)/;
+        $err = $1 if /^  Socket errors: connect 0, read 0, write 0, timeout (\d+)/;
+        $rps = $1 if /^Requests\/sec:   (\d+)/;
     };
     close(I);
-    my $err_rate = int($err * 100 / $conns);
-    print "Attempt $attempt rate $rate rps => $rps rps with $err_rate error rate\n";
-    ($rps, $err_rate);
+    print "Attempt $attempt rate $rate rps => $rps rps with $err error rate\n";
+    ($rps, $err);
 }
